1ad25376555ffefa0fad8655ac87bc12c3b3143f
Ujjwal raj <ujjwalraj@SIMPL-ENG-162.local>
2023-06-09 12:55:10 +0530

    401 auth check tests added

--- src/test/java/cart/FetchCart.java
+++ src/test/java/cart/FetchCart.java
@@ -11,6 +11,7 @@ import org.testng.Assert;
 import io.restassured.path.json.JsonPath;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.Data;
+import com.example.ErrorInfo;
 import com.example.MerchantDetails;
 import io.restassured.specification.RequestSpecification;
 
@@ -18,13 +19,16 @@ import org.junit.jupiter.api.Assertions;
 
 public class FetchCart {
     private RequestSpecification request;
+    private RequestSpecification request_without_cart_session_token;
+    private RequestSpecification request_with_expired_cart_session_token;
 
     private String getCookieValue() {
         return "auth-cookie=" + "visit-id=vs_2QpJBI18C656hDdoXaTNXPl2j90";
     }
 
     private String getCartSessoinTokenValue() {
-        return "c56e32f441a9edcd1414fe3aece7d964";
+        // 07234ea87164b7ea34260b724673b8ce
+        return "07234ea87164b7ea34260b724673b8ce";
     }
 
     @BeforeClass
@@ -33,16 +37,28 @@ public class FetchCart {
         request.baseUri("https://checkout-api.stagingsimpl.com");
         request.basePath("/api/v1/cart");
 
-        RestAssured.requestSpecification = request;
         Header sessionTokenHeader = new Header("cart-session-token",
                 getCartSessoinTokenValue());
         Header cookieHeader = new Header("Cookie", getCookieValue());
         request.header(sessionTokenHeader).header(cookieHeader);
+
+        request_without_cart_session_token = RestAssured.given();
+        request_without_cart_session_token.baseUri("https://checkout-api.stagingsimpl.com");
+        request_without_cart_session_token.basePath("/api/v1/cart");
+        request_without_cart_session_token.header(cookieHeader);
+
+        request_with_expired_cart_session_token = RestAssured.given();
+        request_with_expired_cart_session_token.baseUri("https://checkout-api.stagingsimpl.com");
+        request_with_expired_cart_session_token.basePath("/api/v1/cart");
+        Header expired_sessionTokenHeader = new Header("cart-session-token",
+                "251dd245f0c7a78f01340001ea1556ed");
+        request_with_expired_cart_session_token.header(cookieHeader).header(expired_sessionTokenHeader);
+
     }
 
     @Test
     public void CartFetch_VerifyStatusCode() {
-        Response response = RestAssured.when().get();
+        Response response = request.when().get();
 
         int statusCode = response.getStatusCode();
         int expectedStatusCode = 200;
@@ -51,7 +67,7 @@ public class FetchCart {
 
     @Test
     public void CartFetch_ValidateSuccessField() {
-        Response response = RestAssured.when().get();
+        Response response = request.when().get();
 
         JsonPath responseBody = response.jsonPath();
         boolean successFieldValue = responseBody.getBoolean("success");
@@ -61,12 +77,13 @@ public class FetchCart {
     @Test
     public void CartFetch_ValidateDataField() {
 
-        Response response = RestAssured.when().get();
+        Response response = request.when().get();
         JsonPath responseBody = response.jsonPath();
         Object dataObject = responseBody.get("data");
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             String dataJsonString = objectMapper.writeValueAsString(dataObject);
+
             // Convert the JSON string to a Data object
             Data data = objectMapper.readValue(dataJsonString, Data.class);
             // Assert the fields using getters from the Data object
@@ -119,4 +136,77 @@ public class FetchCart {
 
     }
 
+    @Test
+    public void CartFetch_VerifyStatusCode_cart_session_token_absent() {
+        Response response = request_without_cart_session_token.when().get();
+
+        int statusCode = response.getStatusCode();
+        int expectedStatusCode = 401;
+        Assert.assertEquals(statusCode, expectedStatusCode, "Status code mismatch");
+    }
+
+    @Test
+    public void CartFetch_ValidateSuccessField_cart_session_token_absent() {
+        Response response = request_without_cart_session_token.when().get();
+
+        JsonPath responseBody = response.jsonPath();
+        boolean successFieldValue = responseBody.getBoolean("success");
+        Assert.assertFalse(successFieldValue, "'success' field is not true");
+    }
+
+    @Test
+    public void CartFetch_ValidateErrorField_cart_session_token_absent() {
+
+        Response response = request_without_cart_session_token.when().get();
+        JsonPath responseBody = response.jsonPath();
+        Object dataObject = responseBody.get("error");
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String errorJsonString = objectMapper.writeValueAsString(dataObject);
+            ErrorInfo errorData = objectMapper.readValue(errorJsonString, ErrorInfo.class);
+            // Assert the code and message values
+            Assertions.assertEquals("cart_token_absent", errorData.getCode(), "Code mismatch");
+            Assertions.assertEquals("cart token is absent", errorData.getMessage(), "Message mismatch");
+        } catch (Exception e) {
+            // e.printStackTrace();
+            Assertions.fail("Test failed due to an exception: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void CartFetch_VerifyStatusCode_cart_session_token_expired() {
+        Response response = request_with_expired_cart_session_token.when().get();
+
+        int statusCode = response.getStatusCode();
+        int expectedStatusCode = 401;
+        Assert.assertEquals(statusCode, expectedStatusCode, "Status code mismatch");
+    }
+
+    @Test
+    public void CartFetch_ValidateSuccessField_cart_session_token_expired() {
+        Response response = request_with_expired_cart_session_token.when().get();
+
+        JsonPath responseBody = response.jsonPath();
+        boolean successFieldValue = responseBody.getBoolean("success");
+        Assert.assertFalse(successFieldValue, "'success' field is not true");
+    }
+
+    @Test
+    public void CartFetch_ValidateErrorField_cart_session_token_expired() {
+
+        Response response = request_with_expired_cart_session_token.when().get();
+        JsonPath responseBody = response.jsonPath();
+        Object dataObject = responseBody.get("error");
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String errorJsonString = objectMapper.writeValueAsString(dataObject);
+            ErrorInfo errorData = objectMapper.readValue(errorJsonString, ErrorInfo.class);
+            // Assert the code and message values
+            Assertions.assertEquals("cart_token_expired", errorData.getCode(), "Code mismatch");
+            Assertions.assertEquals("cart token is expired", errorData.getMessage(), "Message mismatch");
+        } catch (Exception e) {
+            // e.printStackTrace();
+            Assertions.fail("Test failed due to an exception: " + e.getMessage());
+        }
+    }
 }
